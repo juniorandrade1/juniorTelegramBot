2c305fdf6406b06a9d09e62c8783005f1c701fdd
Junior <junior.andrade.cco@gmail.com>
2017-07-23 16:38:02 -0300

    Search with name

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-// function downloadAudioFromYoutubeWithId(id, file) {
-//   const yas = require('youtube-audio-server')
-//   console.log(`Downloading ${id} into ${file}...`)
-// yas.downloader
-//   .onSuccess(({id, file}) => {
-//     console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`)
-//     return file;
-//   })
-//   .onError(({id, file, error}) => {
-//     console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
-//     return 'error';
-//   })
-//   .download({id, file})
-// };
-
-// function getAudioFromVideoYoutube(id, file) {
-//     var response = downloadAudioFromYoutubeWithId(id, file);
-//     if(response == 'error') {
-//         console.log('returned error on downloading');
-//         return 'error';
-//     }
-//     return file;
-// }
-
 const TeleBot = require('telebot');
 const bot = new TeleBot('346896437:AAHuKBu4ZcVwwN62xY-VO7lxDVRFVzUeDSk');
-const yas = require('youtube-audio-server')
+const yas = require('youtube-audio-server');
+var search = require('youtube-search');
+
+var optSearch = {
+  maxResults: 1,
+  key: 'AIzaSyBt2PD3Imv4xVNyDLUxzvb53iYuxUHWNpI'
+};
 
 bot.on('text', (msg) => {
-    msg.reply.text('Estamos baixando seu audio');
-    var p = msg.text.split(' ');
-    var id = p[0];
-    var file = p[1];
-    if(file.includes('.mp3') == false) file = file+'.mp3';
-    console.log(`Downloading ${id} into ${file}...`)
-    yas.downloader.onSuccess(({id, file}) => {
-        console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`)
+    search(msg.text, optSearch, function(err, results) {
+      if(err) return console.log(err);
+      console.dir(results[0].id);
+      msg.reply.text('Estamos baixando seu audio');
+      var id = results[0].id;
+      var file = results[0].title;
+      if(file.includes('.mp3') == false) file = file + '.mp3';
+      console.log(`Downloading ${id} into ${file}...`)
+      yas.downloader.onSuccess(({id, file}) => {
+        console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`);
+        msg.reply.text('Encontramos o seu video, já estamos enviando.. só um segundo!');
         msg.reply.audio(file)
-    })
-    .onError(({id, file, error}) => {
-        msg.reply.text('Mandou o ID errado ai jovem, assim não dá pra trabalhar');
+      })
+      .onError(({id, file, error}) => {
         console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
-    })
-    .download({id, file})
+      })
+      .download({id, file})
+    });
 });
 
 bot.start();
